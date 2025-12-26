01c2ae6f6f3625ac2167f68e9667c39a0e13237f
Daniel Assayag <11842639+danass@users.noreply.github.com>
2025-12-26 15:34:30 +0100

    Remove CSP headers - let PostHog work without restrictions

--- next.config.js
+++ next.config.js
@@ -4,29 +4,6 @@ const nextConfig = {
   compress: true,
   allowedDevOrigins: ['192.168.1.50'],
   productionBrowserSourceMaps: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://eu.i.posthog.com https://eu-assets.i.posthog.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: blob: https: http:",
-              "media-src 'self' data: blob: https: http:",
-              "connect-src 'self' https://eu.i.posthog.com https://eu-assets.i.posthog.com https://*.vercel.app wss: https:",
-              "frame-src 'self' https://www.youtube.com https://youtube.com",
-              "worker-src 'self' blob:",
-            ].join('; ')
-          }
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
