b8cfa0152be2e9cfcab10993d45624efb981cbe7
Daniel Assayag <11842639+danass@users.noreply.github.com>
2026-01-08 18:58:29 +0100

    revert: restore sticky toolbar to original position

--- components/editor/UnifiedEditor.js
+++ components/editor/UnifiedEditor.js
@@ -321,7 +321,7 @@ const UnifiedEditor = forwardRef(function UnifiedEditor({
         <div ref={editorContainerRef} className="unified-editor">
             {/* Toolbar */}
             {showToolbar && editor && (
-                <>
+                <div className="mb-4">
                     <GoogleDocsToolbar
                         editor={editor}
                         onOpenIpfsBrowser={showIpfsBrowser ? () => setShowIpfsBrowserModal(true) : undefined}
@@ -331,9 +331,7 @@ const UnifiedEditor = forwardRef(function UnifiedEditor({
                         showSaveButton={showSaveButton}
                         saveButtonIconOnly={saveButtonIconOnly}
                     />
-                    {/* Spacer for fixed toolbar */}
-                    <div className="h-16 mb-4" />
-                </>
+                </div>
             )}
 
             {/* Editor Content */}
